58b616fea21819b10f32e9b10e2d3c24a1c8fd95
riyanwardhana <riyanwardhana2@gmail.com>
2025-02-28 02:24:40 +0700

    add: message element AI response

--- helium.js
+++ helium.js
@@ -3,6 +3,7 @@ const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 
 let userMessage = "";
+const chatHistory = [];
 
 const API_KEY = "AIzaSyA3T2FKL_DhMGY2SCdNx9E5aYmIY2lYiO8";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
@@ -15,10 +16,32 @@ const createMsgElement = (content, ...classes) => {
   return div;
 };
 
-const generateResponse = async () => {
+// MEMBUAT PENGAMBILAN API DAN RESPON DARI AI
+const generateResponse = async (botMsgDiv) => {
+  const textElement = botMsgDiv.querySelector(".message-text");
+
+  // MENAMBAHKAN PESAN USER KE CHAT HISTORY
+  chatHistory.push({
+    role: "user",
+    parts: [{ text: userMessage }],
+  });
+
   try {
-    const response = await fetch();
+    // MENNGIRIM CHAT HISTORY KE API UNTUK DI RESPON
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: chatHistory }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error.message);
+    const responseText = data.candidates[0].content.parts[0].text
+      .replace(/\*\*([^*]+)\*\*/g, "$1")
+      .trim();
+    textElement.textContent = responseText;
   } catch (error) {}
+  console.log(error);
 };
 
 // Handle Form Submit
@@ -40,7 +63,7 @@ const handleFormSubmit = (e) => {
     const botMsgHTML = ` <img src="./img/gemini.svg" class="avatar" /> <p class="message-text">Mikir bentar guyss...</p>`;
     const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
     chatsContainer.appendChild(botMsgDiv);
-    generateResponse();
+    generateResponse(botMsgDiv);
   }, 600);
 };
 
