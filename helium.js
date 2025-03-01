ec34afdc1472c9edc247d54c9f383081f3aa743e
riyanwardhana <riyanwardhana2@gmail.com>
2025-03-02 00:12:20 +0700

    fix: hide suggestions when is chat active

--- helium.js
+++ helium.js
@@ -104,7 +104,7 @@ const handleFormSubmit = (e) => {
 
   promptInput.value = "";
   userData.message = userMessage;
-  document.body.classList.add("bot-responding");
+  document.body.classList.add("bot-responding", "chats-active");
   fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
 
   const userMsgHTML = `
@@ -182,7 +182,15 @@ document.querySelector("#stop-response-btn").addEventListener("click", () => {
 document.querySelector("#delete-chats-btn").addEventListener("click", () => {
   chatHistory.length = 0;
   chatsContainer.innerHTML = "";
-  document.body.classList.remove("bot-responding");
+  document.body.classList.remove("bot-responding", "chats-active");
+});
+
+// Handle suggestions card
+document.querySelectorAll(".suggestions-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    promptInput.value = item.querySelector(".text").textContent;
+    promptForm.dispatchEvent(new Event("submit"));
+  });
 });
 
 promptForm.addEventListener("submit", handleFormSubmit);
