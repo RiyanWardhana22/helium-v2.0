d92740d7b58874618c248e08ff6d1648b815e83c
riyanwardhana <riyanwardhana2@gmail.com>
2025-03-03 01:47:03 +0700

    delete: one item suggestion

--- greeting.js
+++ greeting.js
@@ -1,14 +1,14 @@
 document.getElementById("greeting").textContent = getGreeting();
 
 function getGreeting() {
-  const hour = new Date().getHours(); // Mendapatkan jam saat ini (0-23)
+  const hour = new Date().getHours();
   let greeting;
 
   if (hour >= 5 && hour < 12) {
     greeting = "Pagi 🌞";
   } else if (hour >= 12 && hour < 15) {
     greeting = "Siang ☀️";
-  } else if (hour >= 15 && hour < 18) {
+  } else if (hour >= 15 && hour < 19) {
     greeting = "Sore 🌇";
   } else {
     greeting = "Malam 🌙";
